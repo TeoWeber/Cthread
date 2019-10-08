715d6204e285b7d4df903745b80b3a4df8b9fe70
juviolato <jupviolato@gmail.com>
2019-10-07 22:17:07 -0300

    Scheduler + ccreate

--- src/lib.c
+++ src/lib.c
@@ -6,9 +6,9 @@
 #include "../include/cdata.h"
 
 #define STACK_SS_SIZE 64000
-#define SUCCESS 0;
-#define CIDENTIFY_SIZE_ERROR -1;
-#define EMPTY_QUEUE_ERROR -2;
+#define SUCCESS 0
+#define CIDENTIFY_SIZE_ERROR -1
+#define EMPTY_QUEUE_ERROR -2
 #include <stdio.h>
 #include <string.h>
 #include <ucontext.h>
@@ -17,12 +17,12 @@
 #include "../include/cdata.h"
 
 #define STACK_SS_SIZE 64000
-#define SUCCESS 0;
-#define CIDENTIFY_SIZE_ERROR -1;
-#define EMPTY_QUEUE_ERROR -2;
-#define CREATE_QUEUE_ERROR -3;
+#define SUCCESS 0
+#define CIDENTIFY_SIZE_ERROR -1
+#define EMPTY_QUEUE_ERROR -2
+#define CREATE_QUEUE_ERROR -3
 
-bool main_thread = false;
+int main_thread = 0;
 int next_tid_available = 0;
 FILA2 ready_queue;
 TCB_t* running_queue;
@@ -37,7 +37,7 @@ TCB_t* cmax_prio_pop (PFILA2 pfila) {
     else {
         TCB_t* tcb = GetAtIteratorFila2(pfila);
         int max_prio = tcb->prio;
-        sFilaNode2* max_prio_it = pfila->it;
+        NODE2* max_prio_it = pfila->it;
         while (NextFila2(pfila) != NXTFILA_ENDQUEUE) {
             tcb = GetAtIteratorFila2(pfila);
             if (tcb->prio > max_prio) {
@@ -53,26 +53,20 @@ TCB_t* cmax_prio_pop (PFILA2 pfila) {
 }
 
 int cscheduler () {
+    if (running_queue != NULL)
+    {
+        running_queue->state = PROCST_TERMINO;
+    }
 
-    // Precisa verificar se a thread executando é NULL
-
-    // Se for, então o escalonador foi chamado após uma cwait, cyield ou cjoin (a princípio)
-
-    // Se não for, então o escalonador foi chamado após o término de uma thread
-    // Então o estado da thread executando deverá ser modificado para PROCST_TERMINO
-
-    // Em qualquer caso deverá ser usada a função cmax_prio_pop() para escolher e setar como thread executando
-    // dentre as threads aptos, a thread com maior prioridade
-    // (se houver mais de uma, será a primeira dentre elas)
-    // O estado da mesma, deverá ser modificado para PROCST_EXEC
-    // Então, o contexto da mesma deverá ser selecionado e executado de onde parou
+    running_queue = cmax_prio_pop(&ready_queue);
+    running_queue->state = PROCST_EXEC;
+    swapcontext(&running_queue->context, &schedulerContext);
 
     return SUCCESS;
 }
 
 int cmain_thread_init () {
-    main_thread = true;
-
+    main_thread = 1;
 
     // Inicializando o contexto do escalonador:
     getcontext(&schedulerContext);
@@ -80,7 +74,6 @@ int cmain_thread_init () {
     schedulerContext.uc_stack.ss_sp = (char *) malloc (STACK_SS_SIZE);
     schedulerContext.uc_stack.ss_size = STACK_SS_SIZE;
     makecontext(&schedulerContext, (void (*)(void)) cscheduler , 0);
-
 
     return SUCCESS;
 }
@@ -99,15 +92,12 @@ int ccreate (void* (*start)(void*), void *arg, int prio) {
 	tcb->state = PROCST_APTO;
 	tcb->prio = prio;
 
-	// referência: https://stackoverflow.com/questions/21468529/context-switching-ucontext-t-and-makecontext (revisar)
 	getcontext(&(tcb->context));
 	tcb->context.uc_link = 0;
 	tcb->context.uc_stack.ss_sp = malloc(STACK_SS_SIZE);
 	tcb->context.uc_stack.ss_size = STACK_SS_SIZE;
 	tcb->context.uc_stack.ss_flags = 0;
 	makecontext(&(tcb->context), (void (*)(void))start, 1, arg);
-
-	// verificação se a criação da thread foi bem sucedida
 
 	AppendFila2(&ready_queue, &tcb);
 
@@ -162,16 +152,16 @@ int cwait(csem_t *sem) {
         cmain_thread_init();
     }
 
-    if ((csem_t->count) > 0) {
-        (csem_t->count)--;
+    if ((sem->count) > 0) {
+        (sem->count)--;
         return 0;
     }
     else {
-        (csem_t->count)--;
+        (sem->count)--;
         TCB_t* current_thread = running_queue;
         current_thread->state = PROCST_BLOQ;
         running_queue = NULL;
-        AppendFila2(csem_t->fila, &current_thread);
+        AppendFila2(sem->fila, &current_thread);
         swapcontext(&current_thread->context, &schedulerContext);
         return SUCCESS;
     }
@@ -182,8 +172,8 @@ int csignal(csem_t *sem) {
         cmain_thread_init();
     }
 
-    (csem_t->count)++;
-    TCB_t* tcb = cmax_prio_pop(csem_t->fila);
+    (sem->count)++;
+    TCB_t* tcb = cmax_prio_pop(sem->fila);
     if (tcb == NULL) {
         return EMPTY_QUEUE_ERROR;
     }
